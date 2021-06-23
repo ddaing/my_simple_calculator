20ce9ee0a757442c687b442470cafda7abd71ab4
MinWoo Yoo <ymw@spotu.io>
2021-06-23 23:47:00 +0900

    feat(add) add test adding numbers

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,21 @@ def test_add_two_numbers():
     result = calculator.add(4, 5)
 
     assert result == 9
+
+
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5, 6)
+
+    assert result == 15
+
+
+def test_add_many_numbers():
+    numbers = range(100)
+
+    calculator = SimpleCalculator()
+
+    result = calculator.add(*numbers)
+
+    assert result == 4950
